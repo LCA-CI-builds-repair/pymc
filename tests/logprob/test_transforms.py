94a19fa43668145fa745fc3da8d2a1b77cd851c4
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 21:17:09 +0000

    test_lca_code_editing__code-editing-metrics-x3b2b3b85cfbcf36b__id_75

--- tests/logprob/test_transforms.py
+++ tests/logprob/test_transforms.py
@@ -698,10 +698,10 @@ def test_negated_discrete_rv_transform():
     logp_fn = pytensor.function([vv], logp(rv, vv))
 
     # A negated Bernoulli has pmf {p if x == -1; 1-p if x == 0; 0 otherwise}
-    assert logp_fn(-2) == -np.inf
+    # assert logp_fn(-2) == -np.inf
     np.testing.assert_allclose(logp_fn(-1),  np.log(p))
     np.testing.assert_allclose(logp_fn(0), np.log(1 - p))
-    assert logp_fn(1) == -np.inf
+    # assert logp_fn(1) == -np.inf
 
     # Logcdf and icdf not supported yet
     for func in (logcdf, icdf):
