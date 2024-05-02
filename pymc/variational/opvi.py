196adc76c4ae6cdd305018c8a024bb5a5638bf45
ubuntu <ubuntu@ip-10-0-0-188>
2024-05-02 12:25:56 +0000

    test_lca_code_editing__code-editing-metrics-4e92c9b8a087e580__id_73

--- pymc/variational/opvi.py
+++ pymc/variational/opvi.py
@@ -709,8 +709,7 @@ class Group(WithMemoization):
     def group_for_short_name(cls, name):
         if name.lower() not in cls.__name_registry:
             raise KeyError(
-                "No such group: {!r}, "
-                "only the following are supported\n\n{}".format(name, cls.__name_registry)
+                "No such group: {!r}, only the following are supported\n\n{}".format(name, cls.__name_registry)
             )
         return cls.__name_registry[name.lower()]
 
