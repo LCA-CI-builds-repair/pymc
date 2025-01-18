bf577b91c7b2750931920cba722101a36229529a
ubuntu <ubuntu@ip-10-0-1-44>
2025-01-18 14:26:53 +0000

    waleko__claude-3.5-sonnet_f18c__id_70

--- pymc/variational/opvi.py
+++ pymc/variational/opvi.py
@@ -458,8 +458,8 @@ class Operator:
         self.approx = approx
         if self.require_logq and not approx.has_logq:
             raise ExplicitInferenceError(
-                "%s requires logq, but %s does not implement it"
-                "please change inference method" % (self, approx)
+                f"{self} requires logq, but {approx} does not implement it"
+                "please change inference method"
             )
 
     inputs = property(lambda self: self.approx.inputs)
@@ -510,9 +510,7 @@ class Operator:
         return self.objective_class(self, f)
 
     def __str__(self):  # pragma: no cover
-        return "%(op)s[%(ap)s]" % dict(
-            op=self.__class__.__name__, ap=self.approx.__class__.__name__
-        )
+        return f"{self.__class__.__name__}[{self.approx.__class__.__name__}]"
 
 
 def collect_shared_to_list(params):
